74b8454eb219c693d3360f5e5bf0917ce6efccad
Dzmitry Marudau <dzmitry_marudau@epam.com>
2022-09-17 15:36:55 +0300

    ReactiveSources Solutions: - Split code into execution chunks - Formatting

--- src/main/java/by/dma/reactivegames/exercises/ReactiveSourceOperations.java
+++ src/main/java/by/dma/reactivegames/exercises/ReactiveSourceOperations.java
@@ -22,113 +22,49 @@ public class ReactiveSourceOperations {
     private static final String LINE_DELIMITER = "#".repeat(20);
 
     public static void main(String... args) throws IOException {
-        System.out.printf("%s %s %s%n", LINE_DELIMITER, "intNumbersFlux & userFlux", LINE_DELIMITER);
-        // Print all numbers in the `intNumbersFlux` stream
-        Flux<Integer> integerPublisher = ReactiveSources.intNumbersFlux();
-        integerPublisher.subscribe(element -> System.out.println("consume integerFlux element:" + element));
-        integerPublisher.subscribe(element -> System.out.println("consume again integerFlux element:" + element));
+        //playWithIntNumbersAndUserFlux();
 
-        // Print all users in the `userFlux` stream
-        Flux<User> userPublisher = ReactiveSources.userFlux();
-        userPublisher
-                .subscribe(user -> System.out.println("userFlux element:" + user));
+        //playWithIntNumberMonoAndUserMono();
 
-        // Get all numbers in the `intNumbersFlux` stream into a List and print the list and its size
-/*         List<Integer> integerList = ReactiveSources.intNumbersFlux()
-                .transform(Flux::collectList)
-                .blockFirst(); */
-        List<Integer> integerList = ReactiveSources.intNumbersFlux()
-                .log()
-                .toStream()
-                .toList();
-        System.out.printf("integerList[size=%d] = %s%n", integerList.size(), integerList);
+        //playWithErrorAndCompletionHooks();
 
-        System.out.printf("%s %s %s%n", LINE_DELIMITER, "intNumberMono & userMono", LINE_DELIMITER);
-        // Print the value from `intNumberMono` when it emits
-        Mono<Integer> integerMono = ReactiveSources.intNumberMono();
-        integerMono.subscribe(element -> System.out.printf("consume integerMono element: %d%n", element));
+        //playWIthUnresponsiveFluxAndMono();
 
-        // Get the value from the Mono(`intNumberMono`) into an integer variable
-        Integer number = ReactiveSources.intNumberMono().block();
-        System.out.printf("integer variable from mono: %d%n", number);
-        ReactiveSources.intNumberMono().blockOptional().ifPresent(System.out::println);
+        //playWithIntNumbersFluxAndIntNumbersFluxWithRepeat();
 
-        System.out.printf("%s %s %s%n", LINE_DELIMITER, "error and completion hooks", LINE_DELIMITER);
-        // Subscribe to a flux using the error and completion hooks
-        ReactiveSources.intNumbersFluxWithException()
-                .subscribe(
-                        element -> System.out.printf("consume intNumbersFluxWithException element: %d%n", element),
-                        error -> System.out.printf("intNumbersFluxWithException error: %s%n", error.getMessage()),
-                        () -> System.out.println("intNumbersFluxWithException completed")
-                );
+        //playWIthIntNumbersFluxWithException();
 
-        // Subscribe to a flux using an implementation of BaseSubscriber
-        ReactiveSources.intNumbersFluxWithException()
-                .subscribe(new MyCustomSubscriber<>());
+        playWithTransformation();
 
-        System.out.printf("%s %s %s%n", LINE_DELIMITER, "unresponsiveFlux & unresponsiveMono", LINE_DELIMITER);
-        // Get the value from 'unresponsiveMono' into a String variable but give up after 5 seconds
-/*         String stringFromUnresponsiveMono = ReactiveSources.unresponsiveMono()
-                .doOnError(throwable -> System.out.printf("unresponsiveMono error: %s%n", throwable.getMessage()))
-                .doOnCancel(() -> System.out.println("unresponsiveMono cancelled"))
-                .block(Duration.ofSeconds(5)); */
+        System.out.println("# Press a key to end");
+        System.in.read();
+    }
 
-        // Get the value from 'unresponsiveFlux' into a String list but give up after 5 seconds(use Stream operators)
-        //List<String> listFromUnresponsiveFlux = ReactiveSources.unresponsiveFlux().toStream().toList();
-
-        System.out.printf("%s %s %s%n", LINE_DELIMITER, "intNumbersFlux & intNumbersFluxWithRepeat", LINE_DELIMITER);
-
-        // Print all values from intNumbersFlux that's greater than 5
-        System.out.println("Print all values from intNumbersFlux that's greater than 5");
+    private static void playWithTransformation() {
+        System.out.printf("%s %s %s%n", LINE_DELIMITER, "intNumbersFlux transformation", LINE_DELIMITER);
+        // Print size of intNumbersFlux after the last item returns
+        System.out.println("# Print size of intNumbersFlux after the last item returns");
         ReactiveSources.intNumbersFlux()
-                .log()
-                .filter(element -> element > 5)
-                .subscribe(System.out::println);
+                .count()
+                .subscribe(size -> System.out.printf("intNumbersFlux final size: %d%n", size));
 
-        // Print 10 multiplied by each value from intNumbersFlux that's greater than 5
-        System.out.println("Print 10 multiplied by each value from intNumbersFlux that's greater than 5");
+        // Collect all items of intNumbersFlux into a single list and print it
+        System.out.println("# Collect all items of intNumbersFlux into a single list and print it");
         ReactiveSources.intNumbersFlux()
-                .filter(element -> element > 5)
-                .map(element -> element * 10)
-                .subscribe(System.out::println);
+                .collectList()
+                .subscribe(list -> System.out.printf("intNumbersFlux final list: %s%n", list));
 
-        // Print 10 multiplied by each value from intNumbersFlux for the first 3 numbers emitted that's greater than 5
-        System.out.println("Print 10 multiplied by each value from intNumbersFlux for the first 3 numbers emitted that's greater than 5");
+        // Transform to a sequence of sums of adjacent two numbers
+        System.out.println("# # Transform to a sequence of sums of adjacent two numbers");
         ReactiveSources.intNumbersFlux()
-                .filter(element -> element > 5)
-                .map(element -> element * 10)
-                .take(3)
-                .subscribe(System.out::println);
+                .buffer(2)
+                .map(list -> list.get(0) + list.get(1))
+                .subscribe(x -> System.out.println("two-numbers-adjusted: " + x));
+    }
 
-        // Print each value from intNumbersFlux that's greater than 20. Print -1 if no elements are found
-        System.out.println("Print each value from intNumbersFlux that's greater than 20. Print -1 if no elements are found");
-        ReactiveSources.intNumbersFlux()
-                .filter(element -> element > 20)
-                .defaultIfEmpty(-1)
-                .subscribe(System.out::println);
-
-        // Switch ints from `intNumbersFlux` to the right user from `userFlux`
-        System.out.println("Switch ints from `intNumbersFlux` to the right user from `userFlux`");
-        ReactiveSources.intNumbersFlux()
-                .flatMap(element -> ReactiveSources.userFlux().filter(user -> user.id() == element).take(1))
-                .subscribe(System.out::println);
-
-        // Print only distinct numbers from `intNumbersFluxWithRepeat`
-        System.out.println("Print only distinct numbers from `intNumbersFluxWithRepeat`");
-        ReactiveSources.intNumbersFluxWithRepeat()
-                .distinct()
-                .log()
-                .subscribe(System.out::println);
-
-        // Print from `intNumbersFluxWithRepeat` excluding immediately repeating numbers
-        System.out.println("Print from `intNumbersFluxWithRepeat` excluding immediately repeating numbers");
-        ReactiveSources.intNumbersFluxWithRepeat()
-                .distinctUntilChanged()
-                .log()
-                .subscribe(System.out::println);
-
-        // Print values from intNumbersFluxWithException and print a message when error happens
+    private static void playWIthIntNumbersFluxWithException() {
         System.out.printf("%s %s %s%n", LINE_DELIMITER, "intNumbersFluxWithException", LINE_DELIMITER);
+        // Print values from intNumbersFluxWithException and print a message when error happens
         // version 1: swallow the error
         ReactiveSources.intNumbersFluxWithException()
                 .subscribe(System.out::println, throwable ->
@@ -139,41 +75,130 @@ public class ReactiveSourceOperations {
                 .subscribe(System.out::println); */
 
         // Print values from intNumbersFluxWithException and continue on errors
-        System.out.println("Print values from intNumbersFluxWithException and continue on errors");
+        System.out.println("# Print values from intNumbersFluxWithException and continue on errors");
         ReactiveSources.intNumbersFluxWithException()
                 .onErrorContinue((throwable, element) ->
                         System.out.printf("intNumbersFluxWithException onErrorContinue: %s%n", throwable.getMessage()))
                 .subscribe(System.out::println);
 
         // Print values from intNumbersFluxWithException and when errors happen, replace with a fallback sequence of -1 and -2
-        System.out.println("Print values from intNumbersFluxWithException and when errors happen, replace with a fallback sequence of -1 and -2");
+        System.out.println("# Print values from intNumbersFluxWithException and when errors happen, replace with a fallback sequence of -1 and -2");
         ReactiveSources.intNumbersFluxWithException()
                 .onErrorResume(throwable -> Flux.just(-1, -2))
                 .subscribe(System.out::println);
+    }
 
-        System.out.printf("%s %s %s%n", LINE_DELIMITER, "intNumbersFlux", LINE_DELIMITER);
-        // Print size of intNumbersFlux after the last item returns
-        System.out.println("Print size of intNumbersFlux after the last item returns");
+    private static void playWithIntNumbersFluxAndIntNumbersFluxWithRepeat() {
+        System.out.printf("%s %s %s%n", LINE_DELIMITER, "intNumbersFlux & intNumbersFluxWithRepeat", LINE_DELIMITER);
+        // Print all values from intNumbersFlux that's greater than 5
+        System.out.println("# Print all values from intNumbersFlux that's greater than 5");
         ReactiveSources.intNumbersFlux()
-                .count()
-                .subscribe(size -> System.out.printf("intNumbersFlux size: %d%n", size));
-
-        // Collect all items of intNumbersFlux into a single list and print it
-        System.out.println("Collect all items of intNumbersFlux into a single list and print it");
-        ReactiveSources.intNumbersFlux()
-                .collectList()
-                .subscribe(list -> System.out.printf("intNumbersFlux list: %s%n", list));
-
-        // Transform to a sequence of sums of adjacent two numbers
-        System.out.println("Transform to a sequence of sums of adjacent two numbers");
-        ReactiveSources.intNumbersFlux()
-                .buffer(2)
-                //.map(list -> list.get(0) + list.get(1))
-                .flatMap(list -> list.stream().reduce(Integer::sum).map(Mono::just).orElse(Mono.empty()))
+                .log()
+                .filter(element -> element > 5)
                 .subscribe(System.out::println);
 
-        System.out.println("Press a key to end");
-        System.in.read();
+        // Print 10 multiplied by each value from intNumbersFlux that's greater than 5
+        System.out.println("# Print 10 multiplied by each value from intNumbersFlux that's greater than 5");
+        ReactiveSources.intNumbersFlux()
+                .filter(element -> element > 5)
+                .map(element -> element * 10)
+                .subscribe(System.out::println);
+
+        // Print 10 multiplied by each value from intNumbersFlux for the first 3 numbers emitted that's greater than 5
+        System.out.println("# Print 10 multiplied by each value from intNumbersFlux for the first 3 numbers emitted that's greater than 5");
+        ReactiveSources.intNumbersFlux()
+                .filter(element -> element > 5)
+                .map(element -> element * 10)
+                .take(3)
+                .subscribe(System.out::println);
+
+        // Print each value from intNumbersFlux that's greater than 20. Print -1 if no elements are found
+        System.out.println("# Print each value from intNumbersFlux that's greater than 20. Print -1 if no elements are found");
+        ReactiveSources.intNumbersFlux()
+                .filter(element -> element > 20)
+                .defaultIfEmpty(-1)
+                .subscribe(System.out::println);
+
+        // Switch ints from `intNumbersFlux` to the right user from `userFlux`
+        System.out.println("# Switch ints from `intNumbersFlux` to the right user from `userFlux`");
+        ReactiveSources.intNumbersFlux()
+                .flatMap(element -> ReactiveSources.userFlux().filter(user -> user.id() == element).take(1))
+                .subscribe(System.out::println);
+
+        // Print only distinct numbers from `intNumbersFluxWithRepeat`
+        System.out.println("# Print only distinct numbers from `intNumbersFluxWithRepeat`");
+        ReactiveSources.intNumbersFluxWithRepeat()
+                .distinct()
+                .log()
+                .subscribe(System.out::println);
+
+        // Print from `intNumbersFluxWithRepeat` excluding immediately repeating numbers
+        System.out.println("# Print from `intNumbersFluxWithRepeat` excluding immediately repeating numbers");
+        ReactiveSources.intNumbersFluxWithRepeat()
+                .distinctUntilChanged()
+                .log()
+                .subscribe(System.out::println);
+    }
+
+    private static void playWIthUnresponsiveFluxAndMono() {
+        System.out.printf("%s %s %s%n", LINE_DELIMITER, "unresponsiveFlux & unresponsiveMono", LINE_DELIMITER);
+        // Get the value from 'unresponsiveMono' into a String variable but give up after 5 seconds
+/*         String stringFromUnresponsiveMono = ReactiveSources.unresponsiveMono()
+                .doOnError(throwable -> System.out.printf("unresponsiveMono error: %s%n", throwable.getMessage()))
+                .doOnCancel(() -> System.out.println("# unresponsiveMono cancelled"))
+                .block(Duration.ofSeconds(5)); */
+
+        // Get the value from 'unresponsiveFlux' into a String list but give up after 5 seconds(use Stream operators)
+        //List<String> listFromUnresponsiveFlux = ReactiveSources.unresponsiveFlux().toStream().toList();
+    }
+
+    private static void playWithErrorAndCompletionHooks() {
+        System.out.printf("%s %s %s%n", LINE_DELIMITER, "error and completion hooks", LINE_DELIMITER);
+        // Subscribe to a flux using the error and completion hooks
+        ReactiveSources.intNumbersFluxWithException()
+                .subscribe(
+                        element -> System.out.printf("consume intNumbersFluxWithException element: %d%n", element),
+                        error -> System.out.printf("intNumbersFluxWithException error: %s%n", error.getMessage()),
+                        () -> System.out.println("# intNumbersFluxWithException completed")
+                );
+
+        // Subscribe to a flux using an implementation of BaseSubscriber
+        ReactiveSources.intNumbersFluxWithException()
+                .subscribe(new MyCustomSubscriber<>());
+    }
+
+    private static void playWithIntNumberMonoAndUserMono() {
+        System.out.printf("%s %s %s%n", LINE_DELIMITER, "intNumberMono & userMono", LINE_DELIMITER);
+        // Print the value from `intNumberMono` when it emits
+        Mono<Integer> integerMono = ReactiveSources.intNumberMono();
+        integerMono.subscribe(element -> System.out.printf("consume integerMono element: %d%n", element));
+
+        // Get the value from the Mono(`intNumberMono`) into an integer variable
+        Integer number = ReactiveSources.intNumberMono().block();
+        System.out.printf("integer variable from mono: %d%n", number);
+        ReactiveSources.intNumberMono().blockOptional().ifPresent(System.out::println);
+    }
+
+    private static void playWithIntNumbersAndUserFlux() {
+        System.out.printf("%s %s %s%n", LINE_DELIMITER, "intNumbersFlux & userFlux", LINE_DELIMITER);
+        // Print all numbers in the `intNumbersFlux` stream
+        Flux<Integer> integerPublisher = ReactiveSources.intNumbersFlux();
+        integerPublisher.subscribe(element -> System.out.println("# consume integerFlux element:" + element));
+        integerPublisher.subscribe(element -> System.out.println("# consume again integerFlux element:" + element));
+
+        // Print all users in the `userFlux` stream
+        Flux<User> userPublisher = ReactiveSources.userFlux();
+        userPublisher.subscribe(user -> System.out.println("# userFlux element:" + user));
+
+        // Get all numbers in the `intNumbersFlux` stream into a List and print the list and its size
+/*         List<Integer> integerList = ReactiveSources.intNumbersFlux()
+                .transform(Flux::collectList)
+                .blockFirst(); */
+        List<Integer> integerList = ReactiveSources.intNumbersFlux()
+                .log()
+                .toStream()
+                .toList();
+        System.out.printf("integerList[size=%d] = %s%n", integerList.size(), integerList);
     }
 }
 
@@ -181,7 +206,7 @@ class MyCustomSubscriber<T> extends BaseSubscriber<T> {
 
     @Override
     protected void hookOnSubscribe(Subscription subscription) {
-        System.out.println("hookOnSubscribe");
+        System.out.println("# hookOnSubscribe");
         request(1);
     }
 
@@ -193,6 +218,6 @@ class MyCustomSubscriber<T> extends BaseSubscriber<T> {
 
     @Override
     protected void hookOnError(Throwable throwable) {
-        System.out.println("hookOnError:" + throwable.getMessage());
+        System.out.println("# hookOnError:" + throwable.getMessage());
     }
 }
--- src/main/java/by/dma/reactivegames/exercises/StreamSourceOperations.java
+++ src/main/java/by/dma/reactivegames/exercises/StreamSourceOperations.java
@@ -17,18 +17,18 @@ public class StreamSourceOperations {
         // Use StreamSources.intNumbers() and StreamSources.users()
         System.out.printf("%s %s %s%n", LINE_DELIMITER, "intNumbers & users()", LINE_DELIMITER);
         // Print all numbers in the intNumbers stream
-        System.out.println("Print all numbers in the intNumbers stream");
+        System.out.println("# Print all numbers in the intNumbers stream");
         StreamSources.intNumbers()
                 .forEach(System.out::println);
 
         // Print numbers from intNumbers that are less than 5
-        System.out.println("Print numbers from intNumbers that are less than 5");
+        System.out.println("# Print numbers from intNumbers that are less than 5");
         StreamSources.intNumbers()
                 .filter(num -> num < 5)
                 .forEach(System.out::println);
 
         // Print the second and third numbers in intNumbers that's greater than 5
-        System.out.println("Print the second and third numbers in intNumbers that's greater than 5");
+        System.out.println("# Print the second and third numbers in intNumbers that's greater than 5");
         StreamSources.intNumbers()
                 .filter(num -> num > 5)
                 .skip(1)
@@ -37,7 +37,7 @@ public class StreamSourceOperations {
 
         //  Print the first number in intNumbers that's greater than 5.
         //  If nothing is found, print -1
-        System.out.println("Print the first number in intNumbers that's greater than 5. If nothing is found, print -1");
+        System.out.println("# Print the first number in intNumbers that's greater than 5. If nothing is found, print -1");
         System.out.println(
                 StreamSources.intNumbers()
                         .filter(num -> num > 5)
@@ -45,19 +45,19 @@ public class StreamSourceOperations {
                         .orElse(-1));
 
         // Print first names of all users in userStream
-        System.out.println("Print first names of all users in userStream");
+        System.out.println("# Print first names of all users in userStream");
         StreamSources.users()
                 .map(User::firstName)
                 .forEach(System.out::println);
 
         // Print first names in userStream for users that have IDs from number stream
-        System.out.println("Print first names in userStream for users that have IDs from number stream - variant 1");
+        System.out.println("# Print first names in userStream for users that have IDs from number stream - variant 1");
         StreamSources.users()
                 .filter(user -> StreamSources.intNumbers().anyMatch(num -> num == user.id()))
                 .map(User::firstName)
                 .forEach(System.out::println);
 
-        System.out.println("Print first names in userStream for users that have IDs from number stream - variant 2");
+        System.out.println("# Print first names in userStream for users that have IDs from number stream - variant 2");
         StreamSources.intNumbers()
                 .flatMap(id -> StreamSources.users()
                         .filter(user -> user.id() == id))
